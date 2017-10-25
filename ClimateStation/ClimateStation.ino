827517716d8fa0511ccd78f810b428d3097a2e97
Igor Alentyev <igoralentyev@gmail.com>
2017-10-25 14:32:06 +0300

    Empty

---
+++ ClimateStation/ClimateStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ClimateStation.ino
+ Created:	25.10.2017 14:25:06
+ Author:	igora
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
